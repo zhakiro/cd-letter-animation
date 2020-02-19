9e65a21fe53e860ce87e144194faa5a0614bc236
Mark Zhang <mark.zhang21@gmail.com>
2020-02-19 16:10:06 -0500

    Updates

---
+++ precache-manifest.6e27a8d8d853c21880bd2822c9778cd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e388d3b1152c4cca0fca76c8312c4b49",
+    "url": "/cd-letter-animation/index.html"
+  },
+  {
+    "revision": "f180185c26ea60bdf1ec",
+    "url": "/cd-letter-animation/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "3cfcc2d9c6b095f912fc",
+    "url": "/cd-letter-animation/static/css/main.31908417.chunk.css"
+  },
+  {
+    "revision": "f180185c26ea60bdf1ec",
+    "url": "/cd-letter-animation/static/js/2.367b0208.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cd-letter-animation/static/js/2.367b0208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cfcc2d9c6b095f912fc",
+    "url": "/cd-letter-animation/static/js/main.57229d6e.chunk.js"
+  },
+  {
+    "revision": "a0cb58cd4f4b98a62cc4",
+    "url": "/cd-letter-animation/static/js/runtime-main.f41a2f96.js"
+  }
+]);
